33d69e655dc5eb70f6aed08d4f3cfbb2e32ee8e2
Ignat Insarov <kindaro@gmail.com>
2016-02-27 14:42:09 +0300

    Update 2016-02-27T11:42:06.305Z

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
-angular.module('u', ['ngSanitize']).controller('u_ctrl', function($scope) {
-  $scope.random = [1, 2, 3];
+var m, sanitize_oneline;
+
+m = angular.module('u', ['ngSanitize']);
+
+m.controller('u_ctrl', function($scope) {
+  $scope.elem_owner_id = 'ignat_insarov';
+  $scope.go = function() {
+    return VK.Api.call('users.get', {
+      'user_ids': sanitize_oneline($scope.elem_owner_id)
+    }, function(r) {
+      return VK.Api.call('wall.get', {
+        'owner_id': r.response[0].uid
+      }, function(r) {
+        $scope.posts = r.response.slice(1);
+        return $scope.$apply();
+      });
+    });
+  };
   VK.init({
     'apiId': 5035099
   });
-  return VK.Api.call('wall.get', {
-    'owner_id': 1
-  }, function(r) {
-    $scope.posts = r.response;
-    return $scope.$apply();
-  });
+  return $scope.go();
 });
+
+m.directive("contenteditable", function() {
+  return {
+    restrict: "A",
+    require: "ngModel",
+    link: function(scope, element, attrs, ngModel) {
+      var read;
+      read = function() {
+        return ngModel.$setViewValue(element.html());
+      };
+      ngModel.$render = function() {
+        return element.html(ngModel.$viewValue || "");
+      };
+      return element.bind("blur keyup change", function() {
+        return scope.$apply(read);
+      });
+    }
+  };
+});
+
+sanitize_oneline = function(input) {
+  return input.split('<br>', 1);
+};
